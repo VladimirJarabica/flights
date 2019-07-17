6d1eddb1c7621911ce17790c6ab3589220951821
Honza Liška <jan.liska@mallgroup.com>
2019-07-17 17:00:18 +0200

    removve redux devtool support when on prod

--- src/index.js
+++ src/index.js
@@ -6,7 +6,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import thunk from 'redux-thunk'
 import mainReducer from './store/reducers/index'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose // todo remove for prod
+let composeEnhancers = compose
+if (process.env.NODE_ENV !== 'production') {
+	composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+}
 
 const store = createStore(
 	mainReducer,
